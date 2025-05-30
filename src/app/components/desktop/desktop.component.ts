1a18fa13fbf9a8c3abb5dc91cdf71386d51fe851
Jakub Galinski <info@jakubgalinski.online>
2025-05-30 12:58:27 +0200

    UserNavIconComponent  NotificationIconComponent cooment

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -6,15 +6,21 @@ import { NotepadComponent } from '../notepad/notepad.component';
 import { CalculatorComponent } from '../calculator/calculator.component';
 import { FooterComponent } from "../footer/footer.component";
 
+import { NotificationIconComponent } from '../notification-icon/notification-icon.component';
+import { UserNavIconComponent } from '../user-nav-icon/user-nav-icon.component';
+import { WebContext } from '../../models/WebContext';
+
 @Component({
   selector: 'app-desktop',
   standalone: true,
   imports: [CommonModule, WindowComponent, FooterComponent],
+  // UserNavIconComponent, NotificationIconComponent
   templateUrl: `./desktop.component.html`,
   styleUrl: `./desktop.component.scss`,
 })
 export class DesktopComponent {
   windows$: typeof this.windowService.windows$;
+  // webContext: WebContext = {} as WebContext;
 
   constructor(private windowService: WindowService) {
     this.windows$ = this.windowService.windows$;
